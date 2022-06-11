14e6ac7ac326264d3a4b64ba3633a381f857d8d4
RV <38699422+venchev@users.noreply.github.com>
2022-06-11 18:07:17 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo
-echo WARNING:  This script will remove your VM with Kubernetes and everything. Are you sure, that you want this?
+echo WARNING:  This script will remove your VM with KubeFlow and everything related. 
+echo Are you sure, that you want this?
 echo 
-echo if Yes - the script will await 10 seconds, and you can press Ctrl-C to stop it.
+echo if Yes - the script will await 15 seconds, and you can press Ctrl-C to stop it.
 echo
-echo if you want to continue, just await 10 secs and it will start automatically the REMOVAL process. No undo is possible !!!
+echo if you want to continue, just await 15 secs and it will start automatically. 
+echo No undo is possible !!!
 echo
-echo Counting 10 seconds NOW.
-sleep 10
+echo Counting 15 seconds NOW.
+sleep 15
 echo
-echo 10 seconds have passed. Starting removal of the VM resources and cleanup!
+echo 15 seconds have passed. Starting removal of the VM resources and cleanup!
 echo
 vagrant destroy -f ; rm -rf .vagrant ; rm -rf .git ; vagrant global-status
 echo
