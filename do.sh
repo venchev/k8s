5d86b4af05cf425ba2eade484e7a5607ad4d1a6f
RV <38699422+venchev@users.noreply.github.com>
2022-06-11 17:05:56 +0300

    Update do.sh

--- do.sh
+++ do.sh
@@ -3,7 +3,10 @@ echo creation of Ubuntu 22.04 with Kubernetes 1.24
 vagrant up
 sleep 10
 echo Creating Snapshot !
-vagrant snapshot save k8s-basic-tools-updated-ready-for-K8S-installation
+vagrant snapshot save k8s-updated
 echo
 echo Snapshot was created !
 echo
+vagrant global-status
+echo
+echo Have a nice work!
